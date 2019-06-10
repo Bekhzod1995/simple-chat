1112a9c8e36e835b4410c5d1567d017429badab3
Bekhzod1995 <47529743+Bekhzod1995@users.noreply.github.com>
2019-06-10 21:34:46 +0500

    changed the file name

---
+++ src/List.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actionCreators from '../actions';
+
+
+const mapStateToProps = state => ({
+  channel: state.channelsFromGon.channels,
+});
+
+
+@connect(mapStateToProps, actionCreators)
+class List extends Component {
+  render() {
+    const { channel } = this.props;
+    return (
+      <div>
+        <ul>
+          {channel.map(element => (<li key={element.id}>{element.name}</li>))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default List;
